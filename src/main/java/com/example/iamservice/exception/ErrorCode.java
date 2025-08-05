879e27e4ce0ee0e6daefe8a6fc31bb772451f3d9
tvtdeptrainvt <tvtdeptrainvt@gmail.com>
2025-08-05 17:04:22 +0700

    logout

--- src/main/java/com/example/iamservice/exception/ErrorCode.java
+++ src/main/java/com/example/iamservice/exception/ErrorCode.java
@@ -1,22 +1,26 @@
 package com.example.iamservice.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
-    INVALID_KEY(1001,"lỗi message key"),
-    UNCATEGORIZED(9999, "Lỗi không xác định :>>"),
-    USER_EXISTED(1002, " Email đã tồn tại"),
-    INCORRECT_FORMAT(1003, "Không đúng định dạng"),
-    NOT_BLANK_PASSWORD(1004, " tối thiểu ba ký tự"),
-    USER_NOT_EXISTED(1005, " Email không tồn tại"),
-    UNAUTHENTICATED(1006, "authenticated")
+    INVALID_KEY(1001,"lỗi message key", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNCATEGORIZED(9999, "Lỗi không xác định :>>",  HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002, " Email đã tồn tại", HttpStatus.BAD_REQUEST),
+    INCORRECT_FORMAT(1003, "Không đúng định dạng", HttpStatus.BAD_REQUEST),
+    NOT_BLANK_PASSWORD(1004, " tối thiểu ba ký tự", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1005, " Email không tồn tại", HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(1006, "authenticated", HttpStatus.UNAUTHORIZED)
     ;
     private int code;
     private String message;
+    private final HttpStatusCode statusCode;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 }
